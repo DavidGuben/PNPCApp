91147ee403449304983dafa321e319bed7dc26cf
David Guben <davidguben@Davids-MacBook-Pro.local>
2017-05-03 17:44:08 -0400

    cloned android index from ios index

--- index.android.js
+++ index.android.js
@@ -4,60 +4,84 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   Alert,
-  Image
+  Image,
+  Linking
 } from 'react-native';
+import { Button, Card } from 'react-native-material-design';
 
-const onButtonPress = () => {
-  Alert.alert('Button has been pressed!');
+
+const onButtonPress01 = () => {
+  // Alert.alert('Button has been pressed!');
+  Linking.openURL('https://crm.pawfinity.com/pawsnposeinc/application/').catch(err => console.error('An error occurred', err));
+
+};
+const onButtonPress02 = () => {
+  // Alert.alert('Button has been pressed!');
+  Linking.openURL('https://crm.pawfinity.com/pawsnposeinc/application/').catch(err => console.error('An error occurred', err));
+
+};
+const onButtonPress03 = () => {
+  // Alert.alert('Button has been pressed!');
+  Linking.openURL('https://www.pawsnposecuttery.com').catch(err => console.error('An error occurred', err));
+
 };
 
 export default class pnpcApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-
         <Image
         style={styles.PawPrintTop}
         source={require('./img/PawPrintTop.png')}
         />
 
         <View style={styles.LogoContainer}>
-          <View style={styles.pnpcLogoBox}>
             <Image
             style={styles.pnpcLogo}
             source={require('./img/PNPCLogo.png')}
             />
-          </View>
-          <View style={styles.BNBLogoBox}>
             <Image
             style={styles.BNBLogo}
             source={require('./img/BNBLogo.png')}
             />
-          </View>
+        </View>
+        <View style={styles.phoneNumberBox}>
+          <Image
+          style={styles.PhoneNumber}
+          source={require('./img/PhoneNumber.png')}
+          />
         </View>
 
-        <Image
-        style={styles.PhoneNumber}
-        source={require('./img/PhoneNumber.png')}
-        />
-
         <View style={styles.buttonBox}>
-        <Button
-        style={styles.button01}
-        onPress={onButtonPress}
-        title="Visit Our Website"
-        color="blue"
-        />
+          <Button
+          style={styles.button01}
+          onPress={onButtonPress01}
+          raised={true}
+          text="Groom My Dog"
+          color="blue"
+          />
+
+          <Button
+          style={styles.button01}
+          onPress={onButtonPress02}
+          raised={true}
+          text="Board My Dog"
+          color="blue"
+          />
+
+          <Button
+          style={styles.button01}
+          onPress={onButtonPress03}
+          text="Visit Our Website"
+          color="blue"
+          />
         </View>
 
         <Image
         style={styles.PawPrintBottom}
         source={require('./img/PawPrintBottom.png')}
         />
-
-
 
       </View>
     );
@@ -69,39 +93,40 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   pnpcLogo: {
-
+    right: 20
   },
   pnpcLogoBox: {
-    flex: 1,
 
   },
   BNBLogo: {
-
+    left: 20
   },
-  phoneNumber: {
-    margin: 50,
-    flex: 1
+  phoneNumberBox: {
+    flex: 1,
+    marginBottom: 20
   },
   PawPrintTop: {
-    flex: 1,
-    marginTop: -80
+    bottom: 40
   },
   PawPrintBottom: {
-    flex: 1,
-    marginTop: 50
+    top: 50
   },
-  button01: {},
-  button02: {},
-  buttonBox: {},
-  LogoContainer: {
+  button01: {
     flex: 1,
+    marginBottom: 20
+  },
+  button02: {},
+  buttonBox: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  LogoContainer: {
     flexDirection: 'row',
-    margin: 35
   }
 
 });
